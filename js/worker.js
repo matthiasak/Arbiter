6c633013ed6202d89343c953ad93b4b2788686d7
Matthew Keas <matthiasak@gmail.com>
2015-07-22 08:39:49 -0500

    updating logging

--- js/worker.js
+++ js/worker.js
@@ -1,14 +1,16 @@
 require('babel/polyfill')
 
-global.onmessage = (e) => {
-    (function(log, reset){
-        eval(e.data)
-    }).call(this, function(){
-        Array.prototype.slice.call(arguments).forEach(function(x){
-            postMessage({type: 'log', data: x})
-        })
-    }, function(){
-        postMessage({type: 'reset'})
+let each = (c, fn) => c.forEach(fn)
+let log = (...args) => {
+    let x = args.map((arg) => {
+        if(typeof arg === 'function') return arg.toString()
+        if(arg instanceof Object && arg.__proto__ !== Object.prototype) return arg.toString()
+        return arg
     })
+    postMessage({ type: 'log', data: x.length > 1 ? x : x[0] })
 }
+let reset = () => postMessage({type: 'reset'})
 
+global.onmessage = (e) => {
+    new Function('log', 'reset', e.data)(log, reset)
+}
